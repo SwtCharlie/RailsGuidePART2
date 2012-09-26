fe1ff68a7e1e35e2190306a25135cb8a3072c3a3
Tien Nguyen <tnguyen925@gmail.com>
2012-09-25 22:39:47 -0700

    changed the database adapter

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '0095ff558b879cf91e0ac84babfb609a598c20e931a0f0d6953d7c4b5191d2914cc49fcc35e41b4050a1762d1a643ed69aab71064521f0ff51d33fabc1157fb8'
+Blog::Application.config.secret_token = 'de443ba5a1713d4ef2793e8f3946f4ecf37d2d45e980763728dfc2086009328761c5c5d459aceb184189c98645127e11fd4b481bcc37c8d7f71db821b24d7e09'
